71e3eb5e5132071dae1035a36d3a1748453d9cb4
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-08-10 11:10:43 +0100

    installation of necessary packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
- import image from "next/image";
+ import { Button } from "@/components/ui/button";
+import image from "next/image";
 
  export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+
+      </section>
+       
     </div>
   )
  }
